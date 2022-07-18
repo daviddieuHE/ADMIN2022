34d2d5273b5eccb31f7e21f9516a2c8b83e1d9ed
Debian <debian@vps-2fd7b430.vps.ovh.net>
2022-07-18 13:53:42 +0000

    Dockerfile adaptation, start of postfix conf

--- mail/start-mail.sh
+++ mail/start-mail.sh
@@ -1,3 +1,3 @@
 #sudo docker-compose up --detach
-sudo docker run -d --name=TestMail --hostname mail.l1-1.ephec-ti.be  --restart=always --publish 143:143/tcp --publish 465:465/tcp --publish 587:587/tcp --publish 993:993/tcp test:mail
+sudo docker run -d --name=PostFixDovecot --hostname mail.l1-1.ephec2022.fr  --restart=always --publish 143:143/tcp --publish 465:465/tcp --publish 587:587/tcp --publish 993:993/tcp mail:postfixdovecot
 
